2e728165e8e1e204bbb9d1a1d20b5f3be753b9b0
Balci <h.y.h.balci@amsterdamumc.nl>
2025-02-10 19:20:31 +0100

    simul one.

---
+++ R/simul_1.R
@@ -0,0 +1,112 @@
+# ---------------------------------------------------------
+# 1) Function to generate one dataset for a given scenario
+# ---------------------------------------------------------
+generate_data <- function(n = 250,
+                          is_te_hetero = FALSE,  # toggles homogeneous vs heterogeneous
+                          is_mu_nonlinear = FALSE) {
+  # -- 1. Generate covariates --
+  x1 <- rnorm(n, mean=0, sd=1)
+  x2 <- rnorm(n, mean=0, sd=1)
+  x3 <- rnorm(n, mean=0, sd=1)
+  
+  # x4 is binary
+  x4 <- rbinom(n, size=1, prob=0.5)
+  
+  # x5 is unordered categorical with 3 levels: 1,2,3 (equal prob)
+  # We'll store as integer 1,2,3 but treat it as factor if needed
+  x5_raw <- sample(1:3, size=n, replace=TRUE, prob=c(1/3,1/3,1/3))
+  
+  # Helper function g(x5)
+  g_func <- function(x5) {
+    # x5 takes values in {1,2,3}
+    # g(1)=2, g(2)=-1, g(3)=-4
+    out <- rep(NA, length(x5))
+    out[x5 == 1] <-  2
+    out[x5 == 2] <- -1
+    out[x5 == 3] <- -4
+    return(out)
+  }
+  
+  # -- 2. Prognostic function mu(x) (linear vs. nonlinear) --
+  g_x5  <- g_func(x5_raw)
+  if(!is_mu_nonlinear) {
+    # linear: mu(x) = 1 + g(x5) + x1*x3
+    mu <- 1 + g_x5 + x1*x3
+  } else {
+    # nonlinear: mu(x) = -6 + g(x5) + 6|x3 - 1|
+    mu <- -6 + g_x5 + 6*abs(x3 - 1)
+  }
+  
+  # -- 3. Treatment effect tau(x) (homogeneous vs heterogeneous) --
+  if(!is_te_hetero) {
+    # homogeneous: tau(x) = 3
+    tau_vec <- rep(3, n)
+  } else {
+    # heterogeneous: tau(x) = 1 + 2*x2*x5
+    # x5 is numeric here, but recall it's actually categorical
+    tau_vec <- 1 + 2*x2*(x5_raw)
+  }
+  
+  # -- 4. Compute standard deviation 's' of mu over the sample
+  s <- sd(mu)
+  
+  # -- 5. Propensity function pi(x) = 0.8 Phi(3 mu(x)/s - 0.5 x1) + 0.05 + u_i/10
+  #    where u_i ~ Uniform(0,1)
+  u_i <- runif(n, 0, 1)
+  # standard Normal CDF:
+  Phi <- function(z) pnorm(z, mean=0, sd=1)
+  
+  pi_x <- 0.8 * Phi( (3*mu)/s - 0.5*x1 ) + 0.05 + (u_i / 10)
+  
+  # clamp or ensure pi_x in [0,1], just in case
+  pi_x <- pmin(pmax(pi_x, 0), 1)
+  
+  # -- 6. Treatment assignment z ~ Bernoulli(pi_x)
+  z <- rbinom(n, size=1, prob=pi_x)
+  
+  # -- 7. Outcome Y = mu + z*tau + eps, eps ~ N(0,1)
+  eps <- rnorm(n, 0, 1)
+  y <- mu + z*tau_vec + eps
+  
+  # Return in a data frame
+  data.frame(
+    x1 = x1, 
+    x2 = x2, 
+    x3 = x3,
+    x4 = x4,
+    x5 = factor(x5_raw, levels=c(1,2,3)),  # store as factor
+    z  = z,
+    y  = y,
+    mu = mu,
+    tau = tau_vec
+  )
+}
+
+# ---------------------------------------------------------
+# 2) Generate the 8 scenarios by toggling:
+#    (1) Treatment effect: homogeneous vs. hetero
+#    (2) mu: linear vs. nonlinear
+#    (3) sample size: n=250 vs. n=500
+# ---------------------------------------------------------
+scenarios <- expand.grid(
+  n = c(250, 500),
+  te_hetero = c(FALSE, TRUE),
+  mu_nonlinear = c(FALSE, TRUE)
+)
+
+# We'll store a list of data sets, one per row of 'scenarios'
+sim_data_list <- vector("list", length = nrow(scenarios))
+
+for(i in seq_len(nrow(scenarios))) {
+  par_i <- scenarios[i, ]
+  sim_data_list[[i]] <- generate_data(
+    n             = par_i$n,
+    is_te_hetero  = par_i$te_hetero,
+    is_mu_nonlinear = par_i$mu_nonlinear
+  )
+  cat(sprintf("Generated scenario %d: n=%d, TE_hetero=%s, mu_nonlinear=%s\n",
+              i, par_i$n, par_i$te_hetero, par_i$mu_nonlinear))
+}
+
+# Now 'sim_data_list' contains 8 data frames for the 8 scenarios.
+# Each data frame has columns: (x1, x2, x3, x4, x5, z, y, mu, tau).
