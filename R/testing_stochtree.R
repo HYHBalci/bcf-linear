0c80650fd963530ba014778a337f724c17e30667
HYHBalci <yusufhanbalci@gmail.com>
2025-04-24 19:07:23 +0200

    change the example quickly

--- R/testing_stochtree.R
+++ R/testing_stochtree.R
@@ -1,7 +1,7 @@
 library(stochtree)
 
 source("C:/Users/P094412/Documents/bcf-linear/R/simul_1.R")
-data <- generate_data_2(500, is_te_hetero = TRUE, is_mu_nonlinear = TRUE, seed = 18, RCT = T)
+data <- generate_data_2(500, is_te_hetero = TRUE, is_mu_nonlinear = TRUE, seed = 18, RCT = F)
 general_params_default <- list(
   cutpoint_grid_size = 100, standardize = TRUE, 
   sample_sigma2_global = TRUE, sigma2_global_init = 1, 
@@ -10,12 +10,12 @@ general_params_default <- list(
   adaptive_coding = TRUE, control_coding_init = -0.5, 
   treated_coding_init = 0.5, rfx_prior_var = NULL, 
   random_seed = 1848, keep_burnin = FALSE, keep_gfr = FALSE, 
-  keep_every = 1, num_chains = 2, verbose = TRUE, global_shrinkage = T, unlink = T
+  keep_every = 1, num_chains = 2, verbose = TRUE, global_shrinkage = T, unlink = F, propensity_seperate = T
 )
 
 nbcf_fit <- bcf_linear( X_train = as.matrix(sapply(data[, c(1:6)], as.numeric)),
                         y_train = as.numeric(data$y),
-                        Z_train = as.numeric(data$z), num_gfr = 25, num_burnin =  1000, num_mcmc = 10000, general_params = general_params_default )
+                        Z_train = as.numeric(data$z), num_gfr = 25, num_burnin =  1000, num_mcmc = 4000, general_params = general_params_default )
 
 library(coda)
 # chain_1 <- as.mcmc(nbcf_fit$alpha[1,]*sd(data$y))
@@ -27,6 +27,7 @@ library(coda)
 
 
 # Assuming this is your MCMC sample:
+library(coda)
 for(i in 1:15) {
 chain_1 <- as.mcmc(nbcf_fit$Beta_int[1,,i] * sd(data$y))
 
